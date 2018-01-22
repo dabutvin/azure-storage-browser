fb5059f88d72f27190121073c1281f8e56d54be1
dabutvin <butvinik@outlook.com>
2018-01-21 19:51:35 -0800

    enumerate blob containers

--- AzureStorageBrowser/Activities/BlobActivity.cs
+++ AzureStorageBrowser/Activities/BlobActivity.cs
@@ -11,23 +11,43 @@ using Android.OS;
 using Android.Runtime;
 using Android.Views;
 using Android.Widget;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
 
 namespace AzureStorageBrowser.Activities
 {
     [Activity(Label = "BlobActivity")]
     public class BlobActivity : BaseActivity
     {
-        Account account;
+        CloudBlobClient blobClient;
+
         protected override async void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
 
             SetContentView(Resource.Layout.Blob);
 
-            account = await BlobCache.LocalMachine.GetObject<Account>("selectedAccount");
+            var account = await BlobCache.LocalMachine.GetObject<Account>("selectedAccount");
 
             var accountLabel = FindViewById<TextView>(Resource.Id.account_label);
             accountLabel.Text = account.Name;
+
+            var storageAccount = CloudStorageAccount.Parse($"DefaultEndpointsProtocol=https;AccountName={account.Name};AccountKey={account.Key}");
+
+            blobClient = storageAccount.CreateCloudBlobClient();
+
+            var containers = new List<string>();
+            BlobContinuationToken continuationToken = null;
+            do
+            {
+                var containerSegment = await blobClient.ListContainersSegmentedAsync(continuationToken);
+
+                containers.AddRange(containerSegment.Results.Select(x => x.Name));
+
+                continuationToken = containerSegment.ContinuationToken;
+            } while (continuationToken != null);
+
+            var uiu = "";
         }
     }
 }
